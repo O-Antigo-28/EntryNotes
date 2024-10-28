0126360a55e222497c8287ee1ef09c916cde3bfb
O_Antigo_28 <dadopelapista28@hotmail.com>
2024-10-28 17:58:01 -0300

    alterando a lógica de receber um label em ArraySystemInput e formando o estilo basico do componente

--- src/components/ArraySystemInput/index.tsx
+++ src/components/ArraySystemInput/index.tsx
@@ -1,13 +1,13 @@
-import { useId } from "react"
+import { ReactNode, useId } from "react"
 import "./arraysysteminput.css"
 
-const ArraySystemInput = ({label, value}: {label: string, value: string}) => { 
+const ArraySystemInput = ({children, value}: {children: ReactNode, value: string}) => { 
 
     const arraySystemInputID = useId()
     return (
         <div className="ArraySystemInput">
-            <label className="label__ArraySystemInput" htmlFor={arraySystemInputID}>{label}</label>
-            <input className="input__ArrayS" type="text" name="" id={arraySystemInputID} value={value} />
+            <label className="label__ArraySystemInput" htmlFor={arraySystemInputID}>{children}</label>
+            <input className="input__ArraySystemInput" type="text" name="" id={arraySystemInputID} value={value} />
         </div>
     )
 }
