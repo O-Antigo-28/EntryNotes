24b2bc401fc2ce31249f2561dd3e5c91421210a3
O_Antigo_28 <dadopelapista28@hotmail.com>
2024-11-13 18:06:58 -0300

    adicionado em app.tsx StrictMode na raiz, removendo console.log desnecessário em renderer.js e adicionado a biblioteca react-icons no projeto

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import {BrowserRouter, Routes, Route, HashRouter} from "react-router-dom"
 import ModeChoicePage from './pages/ModeChoicePage';
 import NotExistsPage from './pages/NotExistsPage';
 import AutomaticFileSelection from './pages/AutomaticFileSelection';
-
+import {StrictMode} from 'react'
 import AutomaticPage from './pages/AutomaticPage';
 import ManualPage from './pages/ManualPage';
 import { RecoilRoot } from 'recoil';
@@ -38,15 +38,17 @@ const root = createRoot(document.getElementById("react-container"));
 
 function App(){ 
     return (
-        <RecoilRoot>
-            <HashRouter>
-                <Routes>
-                    {routes.map((route, index) => { 
-                        return <Route path={route.path} key={route.path + index} element={route.element}/>
-                    })}
-                </Routes>
-            </HashRouter>
-        </RecoilRoot>
+        <StrictMode>
+            <RecoilRoot>
+                <HashRouter>
+                    <Routes>
+                        {routes.map((route, index) => { 
+                            return <Route path={route.path} key={route.path + index} element={route.element}/>
+                        })}
+                    </Routes>
+                </HashRouter>
+            </RecoilRoot>
+        </StrictMode>
 
     ) 
 }
--- src/renderer.ts
+++ src/renderer.ts
@@ -28,5 +28,3 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './app';
-
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
