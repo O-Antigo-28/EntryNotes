648104b8bf2cb75426d0f2c8c5afd7be59745f03
O_Antigo_28 <dadopelapista28@hotmail.com>
2024-12-04 22:08:12 -0300

    refatorado CSVFile

--- src/CSVExtractor.ts
+++ src/CSVExtractor.ts
@@ -4,21 +4,28 @@ import {readFile} from "fs/promises"
 import * as XLSX from 'xlsx'
 const DEFAULT_FILE_ENCODING = "utf8"
 
+function isCSVFile(path: string){ 
+  const excelTypeFile = /\.csv$/
+  return excelTypeFile.test(path);
+}
+
 export async function CSVExtractor(path: string, encoding: string= DEFAULT_FILE_ENCODING) :Promise<Object[]> {
 
     try{ 
 
-      const excelFile =  /\.xlsx$/
-      if (excelFile.test(path)){
-        console.log("olha é um arquivo do excel")
+      const notCSVFile = !isCSVFile(path)
+
+      if (notCSVFile){
+        throw new Error(`${path} não é um arquivo CSV`)
       }
-      console.log("path", path)
+      
       const data: Buffer  = await readFile(path)
       const utf8text = iconv.decode(data, encoding)
       const table = Papa.parse(utf8text, {header: true}).data
       table.pop()
       return table
     }
+
     catch(err){
       console.error("erro na leitura do arquivo", err)
       return []
