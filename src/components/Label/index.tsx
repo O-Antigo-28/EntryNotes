4213506186142c0cf1bb1b92e193bc2519ee139b
O_Antigo_28 <dadopelapista28@gmail.com>
2024-12-08 16:42:51 -0300

    refactored LabelElement

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -1,13 +1,30 @@
 
-const LabelElement = ({}: {}) => {
+import React, {ReactNode}from 'react'
+import { Label } from './Label';
+import Price from './Price';
+import './label.css'
 
 
-    return (
-        <div className="LabelElement">
-            <div></div>
 
-        </div>
-    )
+
+const LabelElement = ({description, code, unitOfMeasure, value, promotionalValue}: Label) => {
+
+  if(promotionalValue){
+
+  }
+
+  return(
+    <div className="label">
+      <span className="label__description">{description}</span>
+      <span className="label__code">{code}</span>
+      <Price value={value}/>
+      <span className="label__unit-measure">{unitOfMeasure}</span>
+     
+
+      {promotionalValue && <span className="label__promotional">Promoção</span> }
+      <img src="" alt="" />
+    </div>
+  )
 }
 
 export default LabelElement
