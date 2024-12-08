6774de3a9909020fdcc9e641152841c2b842be17
O_Antigo_28 <dadopelapista28@gmail.com>
2024-12-08 17:16:41 -0300

    added decimais utilities

---
+++ src/decimais.ts
@@ -0,0 +1,17 @@
+export function getDecimal(value: number, fixed?: number):number{
+    const defaultFixed = 2;
+
+    if (fixed){
+        if(fixed < 0){
+            fixed = defaultFixed; 
+        }
+    }
+
+    const precision = 10 ** Math.floor(fixed ?? defaultFixed)
+
+    return value - Math.floor(value * precision)
+    
+}
+export function hasDecimalPlaces(value: number): boolean{
+    return value % 1 !== 0
+}
