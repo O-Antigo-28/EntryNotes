db126791deb1e1f0397860623c3eb1f0d134cf5d
O_Antigo_28 <dadopelapista28@hotmail.com>
2025-08-21 20:34:53 -0300

    trocando url de requisição

--- src/findProductByBarcode.ts
+++ src/findProductByBarcode.ts
@@ -3,7 +3,7 @@ import { IPrice } from "./IPrice"
 type labelInFetch = {cost: number, price: number, description: string, barcode: string}
 export async function findProductByBarcode(barcode: string): Promise<IPrice & ILabel | undefined>{
     try{
-        const result = await fetch(`http://127.0.0.1:3001/product?code=${barcode}`)    
+        const result = await fetch(`http://127.0.0.1:3001/basicproduct?code=${barcode}`)    
         const product: labelInFetch = await result.json()
         return {code: product.barcode, description: product.description, value: product.price, unitOfMeasure: "un", currency: "R$" }
     }catch(e){
